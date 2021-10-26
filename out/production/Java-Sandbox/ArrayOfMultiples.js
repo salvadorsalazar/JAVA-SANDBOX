f6ccb702a99a270b8f5d33dbdba04a16807f0170
salvador salazar <salvador.g.salazar79@gmail.com>
2021-10-25 20:44:56 -0500

    test

---
+++ out/production/Java-Sandbox/ArrayOfMultiples.js
@@ -0,0 +1,9 @@
+function arrayOfMultiples (num, length) {
+    var result = []
+    for (let i = 1; i <= length; i++) {
+        result.push(num*i)
+
+    }
+    return result
+    console.log(arrayOfMultiples(7,5))
+}
